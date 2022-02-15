c7936c4e00cfd350354cb1d9c65b804cf6bc60a5
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2022-02-15 22:19:10 +0900

    WebSidebar 블록 형식으로 변경

--- website/src/components/WebSidebar.js
+++ website/src/components/WebSidebar.js
@@ -2,20 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 const WebSideBlock = styled.div`
-    width: 200px; 
+    width: 150px; 
     min-height:100%; // 최소 높이
 
     position: relative; 
     background: white;
-    // margin: 0px 10px 10px 0px;
-    padding: 10px 20px 15px 30px;
-    border-right: solid gray 1px;
-    // border-radius: 5px; // 모서리 둥굴게
-    // box-shadow: 0 0 3px 0 rgba(0,0,0,100); // 박스 감싸는 그림자
+    margin: 0px 10px 10px 10px;
+    padding: 10px 20px 15px 20px;
+    // border-right: solid gray 1px;
+    border-radius: 5px; // 모서리 둥굴게
+    box-shadow: 0 0 3px 0 rgba(0,0,0,100); // 박스 감싸는 그림자
 
     text-align: left;
     display: flex;
     flex-direction: column;
+    flex-basis:100%;
     gap: 3px;
 
     h2 {
@@ -34,7 +35,7 @@ const WebSideBlock = styled.div`
 // dbg: 카테고리 배열
 var TestCategory = [];
 // dbg: 내용 채우기
-for (var i = 1; i <= 40; i++) {
+for (var i = 1; i <= 20; i++) {
     TestCategory.push(`category${i}`);
 }
 
