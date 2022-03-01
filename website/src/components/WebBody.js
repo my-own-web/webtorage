2df1901538fe6957a9244757e8b01e22b09cc266
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2022-03-01 20:57:54 +0900

    WebBody와 WebSubHeader 파일 분리

--- website/src/components/WebBody.js
+++ website/src/components/WebBody.js
@@ -1,49 +1,14 @@
 import react from "react";
 import styled from "styled-components";
 import WebContent from "./WebContent";
-import { useCurrentCategory } from "./InfoContext";
-import DateSearch from "./DateSearch";
+import WebSubHeader from "./WebSubHeader";
 
 const WebBodyTemplate = styled.div`
-    // width: auto; //100%; // 512px;
-    // height: auto; // 내용에 맞는 크기
-    // min-height:600px; // 최소 크기
-
-    position: relative; //sticky; 
-    // top: 70px;
-    // left: 210px;
-
+    // 사용 안 하는 중
+    position: relative;    
     // background: pink; //white;
-    // padding: 5px;
     // border: solid gray 1px; // dbg
 `
-
-const WebSubHeaderBlock = styled.div`
-    background: white;
-    height: 75px;
-    position: sticky;
-    top: 60px;
-    z-index: 1;
-    border-bottom: solid 1px;
-
-    display: flex;
-    // flex-direction: column;
-    justify-content: space-between;
-    padding: 0px 5px 0px 5px;
-    align-items: center;
-`
-
-function WebSubHeader(){
-    const currentCategory = useCurrentCategory();
-
-    return(
-        <WebSubHeaderBlock>
-            <h2>Category :            {currentCategory}
-            </h2>
-            <DateSearch />
-        </WebSubHeaderBlock>
-    );
-}
 
 function WebBody(){
     return(
---
+++ website/src/components/WebSubHeader.js
@@ -0,0 +1,31 @@
+import react from "react";
+import styled from "styled-components";
+import { useCurrentCategory } from "./InfoContext";
+import DateSearch from "./DateSearch";
+
+const WebSubHeaderBlock = styled.div`
+    background: white;
+    height: 75px;
+    position: sticky;
+    top: 60px;
+    z-index: 1;
+    border-bottom: solid 1px;
+
+    display: flex;
+    // flex-direction: column;
+    justify-content: space-between;
+    padding: 0px 5px 0px 5px;
+    align-items: center;
+`
+
+export default function WebSubHeader(){
+    const currentCategory = useCurrentCategory();
+
+    return(
+        <WebSubHeaderBlock>
+            <h2>           {currentCategory}
+            </h2>
+            <DateSearch />
+        </WebSubHeaderBlock>
+    );
+}
