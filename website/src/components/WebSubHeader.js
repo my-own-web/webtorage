ac33aead7cd82d6b60f4076877050d441b21d036
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2022-03-13 02:03:03 +0900

    SubHeader 모든 원소 한 row에 위치 / SizeButton hover시 색 바뀜

--- website/src/components/WebSubHeader.js
+++ website/src/components/WebSubHeader.js
@@ -1,7 +1,7 @@
 import react from "react";
 import styled from "styled-components";
 import { useCurrentCategory } from "./InfoContext";
-import DateSearch from "./DateSearch";
+import DateButton from "./DateButton";
 
 const WebSubHeaderBlock = styled.div`
     background: white;
@@ -11,19 +11,28 @@ const WebSubHeaderBlock = styled.div`
     z-index: 1;
     border-bottom: solid 1px;
 
-    display: flex;
-    // flex-direction: column;
-    justify-content: space-between;
-    padding: 0px 5px 0px 5px;
+    display: grid;
+    grid-template-columns: 1fr 75px 200px;
+    gap: 5px;
+    // grid-template-rows: 1fr 1fr;
     align-items: center;
+
+    // display: flex;
+    // // flex-direction: column;
+    // justify-content: space-between;
+    // padding: 0px 5px 0px 5px;
 `
 const SizeButton = styled.button`
   height: 25px;
+  width: 75px;
   background: #E5B2FF;
   border: solid purple 1px;
   border-radius: 7px;
   font-size: 12px;
   cursor: pointer;
+  &:hover{
+      background: #dd9ffc;
+  }
 `
 export default function WebSubHeader({boxSize, onClick}){
     const currentCategory = useCurrentCategory();
@@ -33,10 +42,10 @@ export default function WebSubHeader({boxSize, onClick}){
     return(
         <WebSubHeaderBlock>
             <h2>{currentCategory}</h2>
-            <DateSearch />
             <SizeButton onClick={() => {
                 onClick();
             }}>{boxSize ? "작게보기" : "크게보기"}</SizeButton>
+            <DateButton />
         </WebSubHeaderBlock>
     );
 }
