e4172e0e82a204a5417ea8c810981457de2a9a83
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2022-03-22 21:02:36 +0900

    카테고리 검색 창 리셋 버튼 구현

--- website/src/components/CategorySearch.js
+++ website/src/components/CategorySearch.js
@@ -1,19 +1,34 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useCategoryList, useSearchCategoryList } from "./InfoContext";
-
+import { useSearchCategoryList } from "./InfoContext";
+import { MdClear } from 'react-icons/md';
 const CategorySearchBox = styled.div`
-margin: 0 5px 0 0;
+    margin: 0 5px 0 0;
+    // background: grey; // dbg
+
+    display: flex;
+    gap: 5px;
+
+    .clear-icon{
+        visibility: ${props => props.show? 'visible' : 'hidden'};
+        color: red;
+        text: center;
+        margin-top: 1px;
+        cursor: pointer;
+        // weight: bold;
+        font-size: 20px;
+    }
 `
 
 const SearchInput = styled.input`
-width: 120px;
+    width: 120px;
 `
 
 function CategorySearch() {
     const [input, setInput] = useState('');
-    const categoryList = useCategoryList();
     const searchList = useSearchCategoryList();
+    const [show, setShow] = useState(false); // MdClear icon 보일 여부
+    // input에 focus in(onFocus) 하면 show=true, focus out(onBlur) 하면 show=false
 
     // 입력값이 변할 때마다 검색함
     const onChange = (e) => {
@@ -23,9 +38,14 @@ function CategorySearch() {
         searchList(e.target.value);
     };
 
+    const onClick = () => {
+        setInput('');
+    }
+
     return (
-        <CategorySearchBox>
-            <SearchInput placeholder='Search Category' value={input} onChange={onChange} />
+        <CategorySearchBox show={show}>
+            <SearchInput placeholder='Search Category' value={input} onChange={onChange} onFocus={()=>{setShow(true)}} onBlur={()=>{setShow(false)}}/>
+            <MdClear className="clear-icon" onClick={onClick}/>
         </CategorySearchBox>
     );
 }
