3d2e0e2a5dd476df7e6db24d64cb0c975bdf5afd
juk1329 <juk2717an@gmail.com>
2022-03-29 22:45:10 +0900

    date 정보 '-'제거, twitter 태그와 없을 시 대체 정보 받기 구현 중..

--- chrome_extension/src/content.ts
+++ chrome_extension/src/content.ts
@@ -8,11 +8,13 @@ chrome.runtime.onMessage.addListener((message:MessageType, sender) => {
     switch (message.type) {
         case "REQ_TAB":
             const body = document.getElementsByTagName("meta");
+            const temp = document.getElementsByTagName("p");
             let title = "";
             let data_url = "";
             let description = "";
             let image = "";
-            console.log(body[0])
+            console.log(body[0], temp[0].innerText)
+
             for(const prop in body){
                 console.log(prop);
                 if(isNaN(Number(prop))){
@@ -20,25 +22,47 @@ chrome.runtime.onMessage.addListener((message:MessageType, sender) => {
                     break;
                 }
 
-                console.log(body[prop].getAttribute("property"), body[prop].content);
-                if(body[prop].getAttribute("property") != null){
-                    const text = body[prop].getAttribute("property");
-                    if(text){
-                        if(text == "og:title"){
-                            title = body[prop].content;
+                console.log(body[prop].getAttribute("property"), body[prop].getAttribute("name"), body[prop].content);
+                if(body[prop].getAttribute("property") != null || body[prop].getAttribute("name") != null){
+                    if(body[prop].getAttribute("property") != null){
+                        const text = body[prop].getAttribute("property");
+                        if(text){
+                            if(text == "og:title"){
+                                title = body[prop].content;
+                            }
+                            else if(text == "og:url"){
+                                data_url = body[prop].content;
+                            }
+                            else if(text == "og:image"){
+                                image = body[prop].content;
+                            }
+                            else if(text == "og:description"){
+                                description = body[prop].content;
+                            }
                         }
-                        else if(text == "og:url"){
-                            data_url = body[prop].content;
-                        }
-                        else if(text == "og:image"){
-                            image = body[prop].content;
-                        }
-                        else if(text == "og:description"){
-                            description = body[prop].content;
+                    }
+                    else if(body[prop].getAttribute("name") != null){
+                        const text = body[prop].getAttribute("name");
+                        if(text){
+                            if(text == "twitter:title"){
+                                title = body[prop].content;
+                            }
+                            else if(text == "twitter:url"){
+                                data_url = body[prop].content;
+                            }
+                            else if(text == "twitter:image"){
+                                image = body[prop].content;
+                            }
+                            else if(text == "twitter:description"){
+                                description = body[prop].content;
+                            }
                         }
                     }
                 }
-                
+            }
+            if(title === "" && data_url === "" && description === "" && image === "" && document.getElementsByTagName("h1")[0].innerText != null &&  temp[0].innerText != null){
+                title = document.getElementsByTagName("h1")[0].innerText;
+                description = temp[0].innerText;
             }
             console.log("Responding...", title, data_url, description, sender.tab?.id);
 
