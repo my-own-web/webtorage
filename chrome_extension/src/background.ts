de7cc4181c1e24525a0b2b77a9d3fc7a153aeba1
juk1329 <juk2717an@gmail.com>
2022-08-21 20:32:18 +0900

    [FEAT] 크롬 익스텐션 login front-end

--- chrome_extension/src/background.ts
+++ chrome_extension/src/background.ts
@@ -1,6 +1,11 @@
 // This file is ran as a background script
+import * as React from "react";
 import {MessageType} from './types';
 import { TodoApi } from './utils/axios';
+import {useSelector, useDispatch} from 'react-redux';
+import { RootState } from "./modules";
+import {loginState} from "./modules/login";
+
 
 console.log("Hello from background script!")
 
@@ -11,7 +16,14 @@ let description = '';
 let image = '';
 let memo = '';
 
+let loginInfo : loginState = {
+    flag : false,
+    id : ""
+};
+
 const DBconn =async (params: MessageType) => {//tab 정보를 DB에 저장하는 함수
+    console.log("DBconn : ", params);
+
     if(params.type === "DBINFO"){
         try{
             const res = await TodoApi.post('/tabinfo', params);
@@ -30,12 +42,14 @@ const DBconn =async (params: MessageType) => {//tab 정보를 DB에 저장하는
 }
 
 chrome.runtime.onMessage.addListener((message: MessageType) =>{
+
     //두 가지 응답을 받음. 처음엔 팝업창에서 버튼 눌리면 카테고리 저장,
     //두 번째는 content.js에서 탭 정보 불러오는것 -> 이 과정은 먼저 요청을 날리고 받는 것
     switch(message.type){
         case "SIGN_SAVE":
             //category 정보를 저장하는 코드
             //저장하고 다른 탭 정보가 필요하니 content에 쿼리를 날림.
+            loginInfo = message.loginInfo;
             category = message.category;
             memo = message.memo;
             console.log(category, message.category);
@@ -51,6 +65,9 @@ chrome.runtime.onMessage.addListener((message: MessageType) =>{
 
         case "RES_TAB": 
             //content에서 tab 정보를 받는 곳.
+            console.log('step RES_TAB');
+            // const loginState = useSelector((state:RootState) => state.LoginState);
+
             data_url = message.data_url;
             title = message.title;
             description = message.description;
@@ -61,10 +78,10 @@ chrome.runtime.onMessage.addListener((message: MessageType) =>{
             let day = ('0' + today.getDate()).slice(-2);
             const dateString = year + month + day;
             //console.log("check : ", message.title, message. description); 디버그용
-            console.log("completed?: ",category, data_url, title, description, image);
+            console.log("completed?: ",loginInfo, category, data_url, title, description, image);
 
             //message.url, title, description 등 tab정보를 DB로 날리는 곳
-            DBconn({type: 'DBINFO', category: category, data_url:data_url ,title: title, description:description, image:image, date:dateString, memo:memo})
+            DBconn({type: 'DBINFO', clientId : loginInfo.id ,category: category, data_url:data_url ,title: title, description:description, image:image, date:dateString, memo:memo})
             break;
         
         default:
