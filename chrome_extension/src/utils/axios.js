324b054e67f345043449fbc3983a9e58d00fdfa5
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2022-09-25 18:12:14 +0900

    axios withCredentials 옵션 활성화 (쿠키 전달 위해)

--- chrome_extension/src/utils/axios.js
+++ chrome_extension/src/utils/axios.js
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const TodoApi = axios.create({
     //baseURL: process.env.NODE_ENV === "development" ? "http://localhost:3001/api" : "/api"
-    baseURL:"http://localhost:3001/api"
+    baseURL: "http://localhost:3001/api",
+    withCredentials: true
 });
