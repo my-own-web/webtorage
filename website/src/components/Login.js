71855f4f7fbf91c013d13c77c96ee371faca718d
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-02-03 19:22:39 +0900

    [Feat] 새로운 디자인 로그인 페이지에 적용

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -8,28 +8,25 @@ import Cookies from 'universal-cookie';
 import { useContentDispatch } from "./InfoContext";
 
 const LoginBlock = styled.div`
-  width: 700px;
-  //background: #BFDCF4;
-  background: ${lighten(0.05, '#BFDCF4')};
+  width: 500px;
+  background: white;
   border-radius: 12px; /*둥그란 정도*/
-  margin: 0 auto; /* 페이지 중앙에 나타나도록 설정 */
-  margin-top: 60px;
+  margin: 60px auto; /* 페이지 중앙에 나타나도록 설정 */
   justify-content: center;
   display: flex; /*요소들을 자유자제로 위치시키는 속성, block처럼 container 수직 쌓임*/
   flex-direction: column; //Item의 주 축이 수직
-  box-shadow: 8px 5px 20px ${lighten(0.5, '#000')};
+  box-shadow: 0 0 7px ${lighten(0.5, '#000')};
 `;
 
 const GlobalStyle = createGlobalStyle`
   body {
-    //background: #DEF1F7;
-    //background: #e9ecef;
     background: white;
   }
 `;
 
 const LoginHeader = styled.div`
-  padding: 30px 20px 40px 20px;
+padding: 30px 20px 40px 20px;
+  padding: 20px 20px;
   border-bottom: 1px solid #e9ecef;
   h1 {
     margin: 0;
@@ -40,44 +37,44 @@ const LoginHeader = styled.div`
 `;
 
 const LoginBody = styled.div`
-  display: block;
-  padding: 15px 20px;
+  display: flex;
+  flex-direction: column;
   align-items: center;
+  padding: 15px 50px;
   border-radius: 0px 0px 12px 12px;
   h2 {
-    margin: 10px;
+    margin: 0px 0px 0px 20px;
+    align-self: baseline;
   }
 `;
 
 const LoginInput = styled.input`
+  box-sizing: border-box;
   border-radius: 8px;
   border: 1px solid #ada7ab;
   width: 90%;
-  transform: translate(2%);
   height: 50px;
   font-size: 15px;
-  align-items: center;
   justify-content: center;
-  margin: 10px 15px;
+  margin: 5px 0px 20px 0px;
+  padding-left: 10px;
 `;
 
 const LoginButton = styled.button`
-  width: 82%;
-  height: 55px;
-  transform: translate(10%);
+  box-sizing: border-box;
+  width: 90%;
+  height: 50px;
   font-size: 16px;
-  ///border: 1px solid #ada7ab;
-  border: 1px solid #bfbdbd;
-  border-radius: 10px;
-  background: ${darken(0.05, 'whitesmoke')};
-  ///background: #e2e2e2;
-  //background: #BCE9E8;
-  margin-top: 35px;
-  margin-bottom: 35px;
+  border: none;
+  border-radius: 8px;
+  background: whitesmoke;
+  font-weight: bold;
+  cursor: pointer;
+  margin: 20px 0px 20px 0px;
+
 
   &:hover {
-    //background: ${darken(0.05, '#e2e2e2')};
-    background: whitesmoke;
+    background: ${darken(0.05, "whitesmoke")};
   }
 `;
 
@@ -126,7 +123,7 @@ function Login() {
   }
 
   async function UserLogin(userInf) {
-    if (Id === '' && Password === '')
+   if (Id === '' && Password === '')
       alert("아이디와 비밀번호를 입력해주세요");
     else if (Id === '')
       alert("아이디를 입력해주세요");
